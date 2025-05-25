4cf8477623febc88c5a7734347a98ca4fa5ea42d
Camilo Bocanegra <djcamilo0710@hotmail.com>
2025-05-25 15:08:50 -0500

    Variables y tipos de datos - ejercicio de practica

---
+++ src/PerfilUsuario.java
@@ -0,0 +1,15 @@
+public class PerfilUsuario {
+    public static void main(String[] args) {
+        String nombre = "Camilo";
+        int edad = 25;
+        char inicial = 'J';
+        boolean programador = true;
+        double altura = 1.75;
+
+        System.out.println("Hola me llamo: " + nombre);
+        System.out.println("tengo " + edad + " años");
+        System.out.println("mi inicail es: " + inicial);
+        System.out.println("se programar? " + programador);
+        System.out.println("mi altura es " + altura);
+    }
+}
